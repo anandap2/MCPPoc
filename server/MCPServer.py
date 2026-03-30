b6db15b6700391f150ad33d88135b728e22d9cc9
anandap2 <jonathan.anandappa@gmail.com>
2026-03-29 21:36:36 -0400

    updated server code for deployment

--- server/MCPServer.py
+++ server/MCPServer.py
@@ -1,13 +1,16 @@
-
-from fastmcp.tools import tool
 from fastmcp import FastMCP
 
-mcp = FastMCP("POC MCP Server")
-@tool
-def greet(name: str) -> str:
-    """Return a greeting."""
-    return f"Hello, {name}!"
+def create_server():
+    mcp = FastMCP("POC MCP Server")
+
+    @mcp.tool
+    def greet(name: str) -> str:
+        """Return a greeting."""
+        return f"Hello, {name}!"
+
+    return mcp
+
+mcp = create_server()
 
 if __name__ == "__main__":
-    mcp.run()
-
+    mcp.run(transport="http", host="0.0.0.0", port=8081)
